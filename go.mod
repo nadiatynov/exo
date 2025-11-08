4dc113f0c77a1618535419cc817a15341e69d836
knadiath <nadiath.karimou@ynov.com>
2025-11-08 12:02:46 +0100

    Alpbabet

---
+++ go.mod
@@ -0,0 +1,5 @@
+module exo
+
+go 1.25.4
+
+require github.com/01-edu/z01 v0.2.0 // indirect
--- learn.go
+++ learn.go
@@ -4,7 +4,14 @@ import "github.com/01-edu/z01"
 	
 func main() {
 	for i := 'a'; i <= 'z'; i++ { 
-	z01.PrintRune(i)		
+  z01.PrintRune(i)
+   z01.PrintRune(' ') 
 	}
-	z01.PrintRune('\n')
+
+	for j := 'z'; j >='a'; j-- {
+		z01.PrintRune(j)
+		z01.PrintRune(' ')
+		
+	}
+
 }
