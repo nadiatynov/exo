32490403e3bf3dbd47963be662e0e418c63d75e2
knadiath <nadiath.karimou@ynov.com>
2025-11-08 12:09:43 +0100

    Number

--- learn.go
+++ learn.go
@@ -7,9 +7,15 @@ func main() {
   z01.PrintRune(i)
    z01.PrintRune(' ') 
 	}
-
+ z01.PrintRune('\n')
 	for j := 'z'; j >='a'; j-- {
 		z01.PrintRune(j)
+		z01.PrintRune(' ')
+		
+	}
+  z01.PrintRune('\n')
+	for m := '0'; m <= '9'; m++ {
+		z01.PrintRune(m)
 		z01.PrintRune(' ')
 		
 	}
