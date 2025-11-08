83c7731f9f3828dc79a9ae2ee5d249baf5d2acfd
knadiath <nadiath.karimou@ynov.com>
2025-11-08 14:31:29 +0100

    hello

--- fmt.go
+++ fmt.go
@@ -3,6 +3,18 @@ package main
 import "fmt" 
 
 func main() { 
+/*var x int 
+x = 9
+y:=3*/
 
-	
+ var (
+	x int 
+	y string 
+	z bool
+ )
+  x = 17
+  y ="je suis mineur "
+  z = true 
+ fmt.Printf("j'ai : %vans, et %v, Oui c'est %v!",x,y,z)
+
 }
