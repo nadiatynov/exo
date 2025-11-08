4a0b5bc2656f3fd284bf25d0e80306e7ad9c27bc
knadiath <nadiath.karimou@ynov.com>
2025-11-08 20:31:40 +0100

    good

--- operator.go
+++ operator.go
@@ -6,10 +6,12 @@ func main() {
 	var (
 		x int 
 		y int
+		z int 
 	)
 	x = 17
 	y = -3
-	fmt.Println(x + y)
+	z = 6
+	fmt.Println(x + y + z)
 	fmt.Println(x - y)
 	fmt.Println(x * y)
 	fmt.Println(x % y) 
