49cd5983a0faa37ba526a5ac117e8073e8d26aae
knadiath <nadiath.karimou@ynov.com>
2025-11-08 20:24:20 +0100

    operation

---
+++ operator.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var (
+		x int 
+		y int
+	)
+	x = 17
+	y = -3
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x % y) 
+	fmt.Println(x / y)
+}
