5dec10cc61885382933b104070e2a669570c9dc4
knadiath <nadiath.karimou@ynov.com>
2025-11-15 17:05:27 +0100

    brave

--- fmt.go
+++ fmt.go
@@ -7,22 +7,42 @@ func main() {
 x = 9
 y:=3*/
 
- var (
+/* var (
 	x int 
 	y string 
 	z bool 
-	h float32
  )
   x = 17
   y ="je suis mineur "
   z = true 
-  h = 15.66
+  h = 15.669
  fmt.Printf("j'ai : %vans, et %v, Oui c'est %v!",x,y,z)
 
  fmt.Println('\n')
  
- fmt.Println(x , y , z)
+ fmt.Println(x , y , z) 
 
- fmt.Printf("j'ai : %v de moyenne",h)
+
+  for i:= 15; i >=0 ; i-- {
+	if i%2 == 0 {
+		continue
+	}
+	fmt.Println(i)
+  } */
+
+  //var list [n] type 
+  var list [4] int 
+  list[0] = 1
+  list[1] = 6
+  list[2] = 8
+  list[3] = 19
+fmt.Println(list) 
+
+s := [] int{1,2,9}
+for i, h := range s{
+	if i < 2{
+		fmt.Println(h) 
+	} 
+}
 
 }
